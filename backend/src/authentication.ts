97ee550d7ad2c27ca4300a69aa8b6610a129834d
Arrthuur <artgaubil@gmail.com>
2025-08-17 15:55:03 -0400

    feat: add redirect in .env

---
+++ backend/src/authentication.ts
@@ -0,0 +1,63 @@
+import {
+  Products,
+  CountryCode,
+  PlaidApi,
+  Configuration,
+  PlaidEnvironments,
+} from "plaid";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const APP_PORT = process.env.APP_PORT || 8000;
+
+const PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID;
+const PLAID_SECRET = process.env.PLAID_SECRET;
+const PLAID_ENV = process.env.PLAID_ENV || "sandbox";
+const PLAID_REDIRECT_URI =
+  process.env.PLAID_REDIRECT_URI || "https://localhost:3001/";
+
+export class AuthenticationController {
+  private plaidClient: PlaidApi;
+
+  constructor() {
+    console.log(PLAID_CLIENT_ID, PLAID_SECRET);
+    const configuration = new Configuration({
+      basePath: PlaidEnvironments[PLAID_ENV],
+      baseOptions: {
+        headers: {
+          "PLAID-CLIENT-ID": PLAID_CLIENT_ID,
+          "PLAID-SECRET": PLAID_SECRET,
+          "Plaid-Version": "2020-09-14",
+        },
+      },
+    });
+
+    this.plaidClient = new PlaidApi(configuration);
+  }
+
+  public async getPlaidOauthLink() {
+    const request = {
+      user: {
+        // This should correspond to a unique id for the current user.
+        client_user_id: "user-id",
+      },
+      client_name: "Plaid Test App",
+      products: [Products.Transactions],
+      country_codes: [CountryCode.Us],
+      language: "en",
+      redirect_uri: PLAID_REDIRECT_URI,
+    };
+    try {
+      const createTokenResponse =
+        await this.plaidClient.linkTokenCreate(request);
+      console.log("createTokenResponse :>> ", createTokenResponse);
+      const linkToken = createTokenResponse.data.link_token;
+
+      return linkToken;
+    } catch (error) {
+      // handle error
+      console.error("error :>> ", error);
+    }
+  }
+}
