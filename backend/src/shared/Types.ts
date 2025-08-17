6cdf5957a084f2fbcc155d27d6fcded2af099614
Thomas Karatzas <thomasekaratzas@gmail.com>
2025-08-17 17:41:42 -0400

    Fixed types

---
+++ backend/src/shared/Types.ts
@@ -0,0 +1,18 @@
+export interface LinkResponse {
+  linkToken: string;
+}
+
+export interface InvestmentsResponse {
+  holdings: Holding[];
+}
+
+export interface Holding {
+  ticker?: string | null;
+  securityId: string;
+  sector?: string | null;
+  percentage: number;
+}
+
+export interface PlaidLinkResponse {
+  linkToken: string;
+}
